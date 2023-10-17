7d3a7cbd3b8cc59056889c0d708ee8806e3a50b7
Rasmo Wanyama <rasmowanyama@gmail.com>
2023-10-17 08:01:09 +0300

    Docker Container Build File

--- src/build.sh
+++ src/build.sh
@@ -5,7 +5,7 @@ docker build -t sepsis_fastapi -f src/Dockerfile .
 docker images
 
 # Run the Docker container locally
-docker run -p 8000:8000 --name sepsis_fastapi "generated docker image"
+docker run -p 8000:8000 --name sepsis_fastapi 85ae35950e1c
 
 # List running Docker containers
 docker ps
